7ade25b81d53afcc38159221633292f5df958305
TyroneClide <98362062+TyroneClide@users.noreply.github.com>
2023-10-09 13:19:17 -0700

    Display works

--- main.c
+++ main.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct player {
-	char board[10][10];
-    char shots[10][10];
+	char* board;
+	char* shots;
 };
 
+const int W = 10, H = 10;
+
+void initPlayer(struct player plyr);
 void display(struct player plyr);
 
 int main() {
-  struct player plyr = {
-      {{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}},
-      {{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}}
-  }
-  printf(plyr.board[0][0]);
-  display(plyr);
+    struct player plyr = {malloc((W*H)*sizeof(char))};
+	initPlayer(plyr);
+    display(plyr);
 }
 
+//Initializes a player's boards
+void initPlayer(struct player plyr){
+	for(int i = 0; i < W*H; i++){
+    	plyr.board[i] = 'x';
+    	plyr.shots[i] = 'x';
+    }
+}
+
+//Displays the turn screen for a player
 void display(struct player plyr){
-    printf("    1   2   3   4   5   6   7   8   9   10");
-    //printf(plyr.board[0][0]);
+	//Header
+	printf("    ");
+	for(int i = 0; i < W; i++){
+    	if(i < 8) printf("%d  ", i + 1);
+        else printf("%d ", i + 1);
+    }
+    printf("\n\n");    
+    
+    //Displays Both Boards
+    for (int i = 0; i < H; i++) {
+    	printf("%c | ", 'a'+i);
+    	for (int j = 0; j < W; j++){
+            printf("%c  ", plyr.board[i * H + j]);
+			printf("			");            
+            printf("%c  ", plyr.shots[i * H + j]);
+        }
+            
+        printf("\n\n");
+    }
 }
